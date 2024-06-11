72738fa119f016df45ff0699dc298686be6c6aaa
dev-sajid007 <dev.sajid007@gmail.com>
2024-06-11 13:58:21 +0600

    Register & Login Page Mastering Complete

--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Login() {
   return (
     <div>
-      Login
+      <div className="container-login">
+        <div className="row justify-content-center">
+          <div className="col-xl-6 col-lg-12 col-md-9">
+            <div className="card shadow-sm my-5">
+              <div className="card-body p-0">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="login-form">
+                      <div className="text-center">
+                        <h1 className="h4 text-gray-900 mb-4">Login</h1>
+                      </div>
+                      <form className="user">
+                        <div className="form-group">
+                          <input type="email" className="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address" />
+                        </div>
+                        <div className="form-group">
+                          <input type="password" className="form-control" id="exampleInputPassword" placeholder="Password" />
+                        </div>
+                        <div className="form-group">
+                          <div className="custom-control custom-checkbox small" style={{ lineHeight: '1.5rem' }}>
+                            <input type="checkbox" className="custom-control-input" id="customCheck" />
+                            <label className="custom-control-label" htmlFor="customCheck">Remember
+                              Me</label>
+                          </div>
+                        </div>
+                        <div className="form-group">
+                          <a href="index.html" className="btn btn-primary btn-block">Login</a>
+                        </div>
+                        <hr />
+                      </form>
+                      <hr />
+                      <div className="text-center">
+                        <Link className="font-weight-bold small" to="/register">Create an Account!</Link>
+                      </div>
+                      <div className="text-center">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Register() {
   return (
     <div>
-      Register
+      <div className="container-login">
+        <div className="row justify-content-center">
+          <div className="col-xl-10 col-lg-12 col-md-9">
+            <div className="card shadow-sm my-5">
+              <div className="card-body p-0">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="login-form">
+                      <div className="text-center">
+                        <h1 className="h4 text-gray-900 mb-4">Register</h1>
+                      </div>
+                      <form>
+                        <div className="form-group">
+                          <label>First Name</label>
+                          <input type="text" className="form-control" id="exampleInputFirstName" placeholder="Enter First Name" />
+                        </div>
+                        <div className="form-group">
+                          <label>Last Name</label>
+                          <input type="text" className="form-control" id="exampleInputLastName" placeholder="Enter Last Name" />
+                        </div>
+                        <div className="form-group">
+                          <label>Email</label>
+                          <input type="email" className="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address" />
+                        </div>
+                        <div className="form-group">
+                          <label>Password</label>
+                          <input type="password" className="form-control" id="exampleInputPassword" placeholder="Password" />
+                        </div>
+                        <div className="form-group">
+                          <label>Repeat Password</label>
+                          <input type="password" className="form-control" id="exampleInputPasswordRepeat" placeholder="Repeat Password" />
+                        </div>
+                        <div className="form-group">
+                          <button type="submit" className="btn btn-primary btn-block">Register</button>
+                        </div>
+                        <hr />
+                      </form>
+                      <hr />
+                      <div className="text-center">
+                        <Link className="font-weight-bold small" to='/login'>Already have an account?</Link>
+                      </div>
+                      <div className="text-center">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
